af010824546b6410c3a694faf83ef5e89d5d9a7f
Valoeghese <valoeghesemods@gmail.com>
2024-04-03 08:47:10 +1300

    feat: documentation modifications for CommonProperties

--- common/src/main/java/cc/cosmetica/kupe/api/gui/style/CommonProperties.java
+++ common/src/main/java/cc/cosmetica/kupe/api/gui/style/CommonProperties.java
@@ -12,7 +12,7 @@ public final class CommonProperties {
 	}
 
 	/**
-	 * Get the margins of this component. These are the boundaries around the component.
+	 * The margins of the component. These are the boundaries around the component.
 	 * @param vw the screen width.
 	 * @param vh the screen height.
 	 * @return the margins of this component.
@@ -20,7 +20,7 @@ public final class CommonProperties {
 	public static final Style.Property<DimensionsOperator<Margins>> MARGINS = new Style.Property<>((vw, vh) -> Margins.NONE);
 
 	/**
-	 * Get the padding of this component. These are the boundaries within this component.
+	 * The padding of the component. These are the boundaries within this component.
 	 * @param vw the screen width.
 	 * @param vh the screen height.
 	 * @return the padding of this component.
@@ -28,43 +28,32 @@ public final class CommonProperties {
 	public static final Style.Property<DimensionsOperator<Margins>> PADDING = new Style.Property<>((vw, vh) -> Margins.NONE);
 
 	/**
-	 * Get the maximum size of this component. This does not include padding or margins.
+	 * The maximum size of the component. This does not include padding or margins.
 	 * TODO If this is not provided, but the preferred size is, will default to the preferred size.
-	 * @param vw the screen width.
-	 * @param vh the screen height.
-	 * @return the maximum size of this component.
 	 */
-	public static final Style.Property<Optional<Dimensions>> MAXIMUM_SIZE = new Style.Property<>(Optional.empty());
+	public static final Style.Property<DimensionsOperator<Optional<Dimensions>>> MAXIMUM_SIZE = new Style.Property<>((vw, vh) -> Optional.empty());
 
 	/**
-	 * Get the preferred size of this component. This does not include padding or margins.
+	 * The preferred size of the component. This does not include padding or margins.
 	 * This overrides the preferred size due to the component's children.
-	 * @param vw the screen width.
-	 * @param vh the screen height.
-	 * @return the preferred size of this component.
 	 */
-	public static final Style.Property<Optional<Dimensions>> PREFERRED_SIZE = new Style.Property<>(Optional.empty());
+	public static final Style.Property<DimensionsOperator<Optional<Dimensions>>> PREFERRED_SIZE = new Style.Property<>((vw, vh) -> Optional.empty());
 
 	/**
-	 * Get the minimum size of this component. This does not include padding or margins.
+	 * The minimum size of the component. This does not include padding or margins.
 	 * The actual minimum size of the component is the maximum, per-axis, of this and the minimum size due to its
 	 * children.
-	 * @param vw the screen width.
-	 * @param vh the screen height.
-	 * @return the minimum size of this component.
 	 */
-	public static final Style.Property<Optional<Dimensions>> MINIMUM_SIZE = new Style.Property<>(Optional.empty());
+	public static final Style.Property<DimensionsOperator<Optional<Dimensions>>> MINIMUM_SIZE = new Style.Property<>((vw, vh) -> Optional.empty());
 
 	/**
-	 * Whether to preserve the shape (ratio of x and y) of this component, when resizing.
-	 * @return whether the shape of this component should be preserved.
+	 * Whether to preserve the shape (ratio of x and y) of the component, when resizing.
 	 */
 	public static final Style.Property<Boolean> PRESERVE_SHAPE = new Style.Property<>(false);
 
 	/**
 	 * Get the amount this object flexes in collections. That is, when objects are being made to fill the space of the
-	 * collection, allocate this proportion to this component.
- 	 * @return the amount this object flexes in collections. If 0, this component will not flex.
+	 * collection, allocate this proportion to this component. If 0, this component will not flex.
 	 */
 	public static final Style.Property<Integer> FLEX = new Style.Property<>(0);
 
