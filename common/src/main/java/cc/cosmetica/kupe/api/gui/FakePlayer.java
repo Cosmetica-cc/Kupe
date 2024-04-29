e720687b7bad71631f7b43dc8e71bf240ad99bee
Valoeghese <valoeghesemods@gmail.com>
2024-04-29 12:43:32 +1200

    wip: more work on attachment and fake player

--- common/src/main/java/cc/cosmetica/kupe/api/gui/FakePlayer.java
+++ common/src/main/java/cc/cosmetica/kupe/api/gui/FakePlayer.java
@@ -1,6 +1,7 @@
 package cc.cosmetica.kupe.api.gui;
 
-import cc.cosmetica.kupe.impl.fakeplayer.AttachmentsImpl;
+import cc.cosmetica.kupe.api.Canvas;
+import com.google.common.base.Preconditions;
 import net.minecraft.resources.ResourceLocation;
 import org.jetbrains.annotations.NotNull;
 import org.jetbrains.annotations.Nullable;
@@ -18,8 +19,11 @@ public class FakePlayer extends Component {
 	 * Display a fake player with the given skin texture. No attachments by default.
 	 * @param skin the resource location of the skin texture.
 	 */
-	public FakePlayer(ResourceLocation skin) {
+	public FakePlayer(@NotNull ResourceLocation skin) {
+		Preconditions.checkNotNull(skin, "Cannot provide a null skin.");
+
 		this.uuid = null;
+		this.skin = skin;
 	}
 
 	/**
@@ -28,22 +32,23 @@ public class FakePlayer extends Component {
 	 * @param uuid the uuid of the player to render.
 	 */
 	public FakePlayer(@NotNull UUID uuid) {
-		this.uuid = uuid;
+		Preconditions.checkNotNull(uuid, "Cannot provide a null UUID.");
 
-		for (Attachment<?> attachment : AttachmentsImpl.getAttachments()) {
-			attachment.inferDefaultConfiguration(uuid);
-		}
+		this.uuid = uuid;
+		this.skin = null;
 	}
 
 	private final @Nullable UUID uuid;
+	private final @Nullable ResourceLocation skin;
 
 	/**
 	 * Configure the given attachment on this FakePlayer.
 	 * @param attachment the attachment to configure on this FakePlayer.
 	 * @param configuration the configuration to provide for this attachment.
+	 * @return this fake player.
 	 */
-	public <T> void configureAttachment(Attachment<T> attachment, T configuration) {
-
+	public <T> FakePlayer configureAttachment(Attachment<T> attachment, T configuration) {
+		return this;
 	}
 
 	/**
@@ -73,6 +78,18 @@ public class FakePlayer extends Component {
 		// CapeAttachment -- requires ResourceLocation or can look up via UUID
 		// ElytraAttachment -- "
 		// NameTagAttachment -- String or look up via UUID
-		T inferDefaultConfiguration(@Nullable UUID uuid);
+
+		/**
+		 * Render this attachment on the fake player.
+		 * @param canvas the canvas environment for rendering.
+		 * @param configuration the configuration.
+		 */
+		void render(Canvas canvas, T configuration);
+
+		/**
+		 * Get the user configuration. This is called every frame, so can change dynamically.
+		 * @return the configuration for the given user.
+		 */
+		T getUserConfiguration(UUID uuid);
 	}
 }
