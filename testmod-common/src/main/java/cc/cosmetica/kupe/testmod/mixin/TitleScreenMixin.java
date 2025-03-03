607a617033b8c381b11dc2714822e31e1268448c
valoeghese <46117728+valoeghese@users.noreply.github.com>
2025-03-04 09:29:34 +1300

    chore: Update License Year

--- testmod-common/src/main/java/cc/cosmetica/kupe/testmod/mixin/TitleScreenMixin.java
+++ testmod-common/src/main/java/cc/cosmetica/kupe/testmod/mixin/TitleScreenMixin.java
@@ -1,6 +1,6 @@
 /*
  * Kupe Testmod - Test and Example code for usage of the Kupe Library.
- * Written in 2024 by Cosmetica Contributors
+ * Written in 2024-2025 by Cosmetica Contributors
  * To the extent possible under law, the author(s) have dedicated all copyright and related and neighboring rights to this software to the public domain worldwide. This software is distributed without any warranty.
  * You should have received a copy of the CC0 Public Domain Dedication along with this software. If not, see <http://creativecommons.org/publicdomain/zero/1.0/>.
  */
