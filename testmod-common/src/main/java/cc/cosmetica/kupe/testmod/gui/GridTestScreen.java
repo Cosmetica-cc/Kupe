4e8d4dbc84f6d2afc1250f6230fb8a37b336d8cf
valoeghese <46117728+valoeghese@users.noreply.github.com>
2025-03-08 19:17:36 +1300

    test: Grid behaviour test

--- testmod-common/src/main/java/cc/cosmetica/kupe/testmod/gui/GridTestScreen.java
+++ testmod-common/src/main/java/cc/cosmetica/kupe/testmod/gui/GridTestScreen.java
@@ -10,6 +10,15 @@ package cc.cosmetica.kupe.testmod.gui;
 import cc.cosmetica.kupe.api.ResourceKey;
 import cc.cosmetica.kupe.api.Screen;
 import cc.cosmetica.kupe.api.gui.Component;
+import cc.cosmetica.kupe.api.gui.Div;
+import cc.cosmetica.kupe.api.gui.Grid;
+import cc.cosmetica.kupe.api.gui.style.Style;
+import cc.cosmetica.kupe.api.gui.style.Stylesheet;
+import org.jetbrains.annotations.Nullable;
+
+import java.util.OptionalInt;
+
+import static cc.cosmetica.kupe.api.gui.style.CommonProperties.*;
 
 /**
  * Test case for {@link cc.cosmetica.kupe.api.gui.Grid}.
@@ -21,8 +30,37 @@ public class GridTestScreen extends Screen {
 
     @Override
     protected Component[] buildScreen() {
-        return new Component[0];
+        return new Component[] {
+                new Grid(
+                        Box(), Box2(), Box(), Box(), Box2()
+                ).tag("x")
+        };
     }
 
-    public static final ResourceKey ID = new ResourceKey("kupe-test", "grid");
+    @Override
+    public @Nullable Stylesheet getStylesheet() {
+        return super.getStylesheet()
+                .tag("x", Style.create()
+                        .set(Grid.COLUMN_GAP, 2)
+                        .set(Grid.ROW_GAP, 2)
+                        .set(WIDTH, percent(50, 0))
+                        .set(HEIGHT, percent(0, 80)))
+                .tag("box", Style.create()
+                        .set(BACKGROUND_COLOUR, OptionalInt.of(0xd3c5a2))
+                        .set(WIDTH, fixed(OptionalInt.of(50)))
+                        .set(HEIGHT, fixed(OptionalInt.of(50))))
+                .tag("box2", Style.create()
+                        .set(BACKGROUND_COLOUR, OptionalInt.of(0x7d87e8))
+                        .set(WIDTH, fixed(OptionalInt.of(30)))
+                        .set(HEIGHT, fixed(OptionalInt.of(30))));
+    }
+
+    private static Component Box() {
+        return new Div().tag("box");
+    }
+    private static Component Box2() {
+        return new Div().tag("box2");
+    }
+
+    public static final ResourceKey ID = new ResourceKey("kupe_test", "grid");
 }
